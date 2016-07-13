aaa888df92f6ad59ddf710a11b267936b6e3e06c
Ross Bower <ross@ross-mbp.wireless.dartmouth.edu>
2016-07-13 13:34:55 -0400

    finished basic

--- src/index.js
+++ src/index.js
@@ -1,10 +1,51 @@
-// change require to es6 import style
-import $ from 'jquery';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import youtubeSearch from './youtube-api';
 import './style.scss';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import debounce from 'lodash.debounce';
 
-let num = 0;
-// http://www.w3schools.com/jsref/met_win_setinterval.asp
-setInterval(() => {
-  num++;
-  $('#main').html(`You\'ve been on this page for ${num} seconds!`);
-}, 1000);
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+
+    this.search('pixar');
+
+    this.search = debounce(this.search, 300);
+  }
+
+  search(text) {
+    youtubeSearch(text).then(videos => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0],
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="search_bar"><SearchBar onSearchChange={text => this.search(text)} /></div>
+        <div id="video-section">
+          <VideoDetail video={this.state.selectedVideo} />
+          <VideoList onVideoSelect={selectedVideo => this.setState({ selectedVideo })} videos={this.state.videos} />/>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('main'));
+
+youtubeSearch('pixar').then(videos => {
+  console.log(videos);
+});
